f3caf49d5f22d43476f38b4f47b56951da550bc3
howesrichard <82419531+howesrichard@users.noreply.github.com>
2026-01-11 22:59:31 +0000

    Remove 2025 content

--- Seminar_2/get_commentary.py
+++
@@ -1,6 +0,0 @@
-# Read commentary from a text file
-def read_commentary(file_path):
-    commentary = ""
-    with open(file_path, "r") as file:
-        commentary = file.read()
-    return commentary
